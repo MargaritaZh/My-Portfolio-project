f338d38e4ebac2bde0f01e9acef4a23b2ed614d2
MargaritaZh <margaritazhitkevich@gmail.com>
2024-04-10 23:12:48 +0300

    WorkHistory- stylisation

--- src/assets/layout/sections/workHistory/WorkHistory.tsx
+++ src/assets/layout/sections/workHistory/WorkHistory.tsx
@@ -5,41 +5,51 @@ import {
 } from "../../../../components/educationAndWorkHistoryCard/EducationAndWorkHistoryCard";
 import {SectionTitle} from "../../../../components/SectionTitle";
 import {SectionTextDescription} from "../../../../components/SectionTextDescription";
-import {FlexWrapper} from "../../../../components/FlexWrapper";
+import {StyledSection} from "../../../../components/StyledSection";
+import {SectionWrapperTitleAndText} from "../../../../components/SectionWrapperTitleAndText";
+import {theme} from "../../../../styles/Theme";
 
 export const WorkHistory = () => {
     return (
-        <StyledWorkHistory>
-            <SectionTitle>Work History</SectionTitle>
-            <SectionTextDescription>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. lorem ipsum</SectionTextDescription>
-            <FlexWrapper direction={"column"} align={"center"} >
+        <StyledSection>
+            <SectionWrapperTitleAndText>
+                <SectionTitle>Work History</SectionTitle>
+                <SectionTextDescription>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit
+                    officia consequat duis enim velit mollit. lorem ipsum</SectionTextDescription>
+            </SectionWrapperTitleAndText>
+            <WrapperCards>
                 <EducationAndWorkHistoryCard
                     leftTitle={"Lead Web Designer"}
                     status={"Student"}
                     periodOfStudy={"Jan 1016 - Dec 2021"}
-                    rightTitle={"Jan 1016 - Dec 2021"}
+                    rightTitle={"Certificate Of Web Training"}
                     rightTitleDescription={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet dapibus nibh ut faucibus nunc, egestas id amet porttitor. Pulvinar quisque sed amet, nulla nunc. Eleifend sodales posuere fusce tempus etiam et pellentesque. Molestie risus enim neque eget dui."}/>
                 <EducationAndWorkHistoryCard
                     leftTitle={"Junior Web Designer"}
                     status={"Student"}
                     periodOfStudy={"Jan 1016 - Dec 2021"}
-                    rightTitle={"Jan 1016 - Dec 2021"}
+                    rightTitle={"Certificate Of Web Training"}
                     rightTitleDescription={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet dapibus nibh ut faucibus nunc, egestas id amet porttitor. Pulvinar quisque sed amet, nulla nunc. Eleifend sodales posuere fusce tempus etiam et pellentesque. Molestie risus enim neque eget dui."}/>
                 <EducationAndWorkHistoryCard
                     leftTitle={"Senior Web Designer"}
                     status={"Student"}
                     periodOfStudy={"Jan 1016 - Dec 2021"}
-                    rightTitle={"Jan 1016 - Dec 2021"}
+                    rightTitle={"Certificate Of Web Training"}
                     rightTitleDescription={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet dapibus nibh ut faucibus nunc, egestas id amet porttitor. Pulvinar quisque sed amet, nulla nunc. Eleifend sodales posuere fusce tempus etiam et pellentesque. Molestie risus enim neque eget dui."}/>
-            </FlexWrapper>
-        </StyledWorkHistory>
+            </WrapperCards>
+        </StyledSection>
     );
 };
 
-const StyledWorkHistory = styled.section`
-    background-color: #91cccc;
-    height: 100vh;
+const WrapperCards=styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background-color: ${theme.colors.secondaryBg};
+    gap: 28px;
+    padding: 47px 29px 38px 37px;
 `
+
 
 
 
