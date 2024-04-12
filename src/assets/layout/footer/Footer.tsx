afe31ccd39ade338585c05edab9f6feeaa71eb90
MargaritaZh <margaritazhitkevich@gmail.com>
2024-04-13 00:48:37 +0300

    Footer stylisation

--- src/assets/layout/footer/Footer.tsx
+++ src/assets/layout/footer/Footer.tsx
@@ -2,23 +2,32 @@ import React from 'react';
 import styled from "styled-components";
 import {Icon} from "../../../components/icon/Icon";
 import {Container} from "../../../components/Container";
+import {theme} from "../../../styles/Theme";
+import {FlexWrapper} from "../../../components/FlexWrapper";
 
 export const Footer = () => {
     return (
         <StyledFooter>
             <Container>
-                <Icon iconId={"copyright"} height={"24"} width={"24"} viewBox={"0 0 24 24"}/>
-                <Copyright>2021 All Rights Reserved.Ojjomedia</Copyright>
+                <FlexWrapper justify={"center"} align={"center"}>
+                    <Icon iconId={"copyright"} height={"24"} width={"24"} viewBox={"0 0 24 24"}/>
+                    <Copyright>2021 All Rights Reserved.Ojjomedia</Copyright>
+                </FlexWrapper>
             </Container>
         </StyledFooter>
     );
 };
 
 const StyledFooter = styled.footer`
-    background-color: yellow;
-    display: flex;
-    justify-content: center;
+    background-color: ${theme.colors.secondaryBg};
+    padding: 18px 0;
 `
 
-const Copyright = styled.small``
+const Copyright = styled.small`
+
+    font-size: 15px;
+    font-weight: 400;
+    line-height: 1.6;
+    margin-left: 20px;
+`
 
