3c6780fffd4b918bfee6b25ed027c1c93fc99fe4
MargaritaZh <margaritazhitkevich@gmail.com>
2024-04-13 22:57:01 +0300

    ExtraSkillsCard - stylisation

--- src/assets/layout/sidebar/extraSkillsCard/ExtraSkillsCard.tsx
+++ src/assets/layout/sidebar/extraSkillsCard/ExtraSkillsCard.tsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import styled from "styled-components";
 import {Icon} from "../../../../components/icon/Icon";
+import {theme} from "../../../../styles/Theme";
 
-type ExtraSkillsCardPropsType={
-    title:string
+type ExtraSkillsCardPropsType = {
+    title: string
     items: Array<string>
 }
 
-export const ExtraSkillsCard = (props:ExtraSkillsCardPropsType) => {
+export const ExtraSkillsCard = (props: ExtraSkillsCardPropsType) => {
     return (
         <StyledExtraSkillsCard>
             <Title>{props.title}</Title>
 
-            {props.items.map((item,index)=>{
+            {props.items.map((item, index) => {
                 return (
-                    <div key={index}>
+                    <Wrapper key={index}>
                         <Icon iconId={"skills"} width={"15"} height={"15"} viewBox={"0 0 15 15"}/>
-                        <span>{item}</span>
-                    </div>
+                        <Text>{item}</Text>
+                    </Wrapper>
                 )
             })}
 
@@ -25,6 +26,32 @@ export const ExtraSkillsCard = (props:ExtraSkillsCardPropsType) => {
     );
 };
 
-const StyledExtraSkillsCard=styled.div``
+const StyledExtraSkillsCard = styled.div`
+    min-width: 220px;
+    padding: 25px 0;
+    border-bottom: 2px solid ${theme.colors.primaryBg};
 
-const Title=styled.h3``
+`
+
+const Title = styled.h3`
+    font-size: 18px;
+    font-weight: 500;
+    margin-bottom: 15px;
+`
+
+const Wrapper=styled.div`
+    display: flex;
+    gap: 15px;
+
+    &-div {
+        margin-top: 5px;
+    }
+
+`
+
+const Text=styled.span`
+    color: ${theme.colors.fontDescription};
+    font-size: 15px;
+    font-weight: 400;
+    line-height: 1.6;
+    `
