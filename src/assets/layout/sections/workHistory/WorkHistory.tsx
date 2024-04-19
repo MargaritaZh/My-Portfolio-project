71d1c85561a814636ebf5e62c9eb022c2ea000e9
MargaritaZh <margaritazhitkevich@gmail.com>
2024-04-20 00:57:48 +0300

    Refactoring (map) - WorkHistory

--- src/assets/layout/sections/workHistory/WorkHistory.tsx
+++ src/assets/layout/sections/workHistory/WorkHistory.tsx
@@ -9,6 +9,32 @@ import {SectionTitle} from "../../../../components/SectionTitle";
 import {WrapperEducationAndWorkHistoryCards} from "../../../../components/WrapperEducationAndWorkHistoryCards";
 import {Container} from "../../../../components/Container";
 
+
+const workHistoryData = [
+    {
+        leftTitle: "Lead Web Designer",
+        status: "Student",
+        periodOfStudy: "Jan 1016 - Dec 2021",
+        rightTitle: "Certificate Of Web Training",
+        rightTitleDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet dapibus nibh ut faucibus nunc, egestas id amet porttitor. Pulvinar quisque sed amet, nulla nunc. Eleifend sodales posuere fusce tempus etiam et pellentesque. Molestie risus enim neque eget dui."
+    },
+    {
+        leftTitle: "Junior Web Designer",
+        status: "Student",
+        periodOfStudy: "Jan 1016 - Dec 2021",
+        rightTitle: "Certificate Of Web Training",
+        rightTitleDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet dapibus nibh ut faucibus nunc, egestas id amet porttitor. Pulvinar quisque sed amet, nulla nunc. Eleifend sodales posuere fusce tempus etiam et pellentesque. Molestie risus enim neque eget dui."
+    },
+    {
+        leftTitle: "Senior Web Designer",
+        status: "Student",
+        periodOfStudy: "Jan 1016 - Dec 2021",
+        rightTitle: "Certificate Of Web Training",
+        rightTitleDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet dapibus nibh ut faucibus nunc, egestas id amet porttitor. Pulvinar quisque sed amet, nulla nunc. Eleifend sodales posuere fusce tempus etiam et pellentesque. Molestie risus enim neque eget dui."
+    },
+]
+
+
 export const WorkHistory = () => {
     return (
         <StyledSection>
@@ -20,24 +46,18 @@ export const WorkHistory = () => {
                         officia consequat duis enim velit mollit. lorem ipsum</SectionDescription>
                 </SectionTitle>
                 <WrapperEducationAndWorkHistoryCards>
-                    <EducationAndWorkHistoryCard
-                        leftTitle={"Lead Web Designer"}
-                        status={"Student"}
-                        periodOfStudy={"Jan 1016 - Dec 2021"}
-                        rightTitle={"Certificate Of Web Training"}
-                        rightTitleDescription={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet dapibus nibh ut faucibus nunc, egestas id amet porttitor. Pulvinar quisque sed amet, nulla nunc. Eleifend sodales posuere fusce tempus etiam et pellentesque. Molestie risus enim neque eget dui."}/>
-                    <EducationAndWorkHistoryCard
-                        leftTitle={"Junior Web Designer"}
-                        status={"Student"}
-                        periodOfStudy={"Jan 1016 - Dec 2021"}
-                        rightTitle={"Certificate Of Web Training"}
-                        rightTitleDescription={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet dapibus nibh ut faucibus nunc, egestas id amet porttitor. Pulvinar quisque sed amet, nulla nunc. Eleifend sodales posuere fusce tempus etiam et pellentesque. Molestie risus enim neque eget dui."}/>
-                    <EducationAndWorkHistoryCard
-                        leftTitle={"Senior Web Designer"}
-                        status={"Student"}
-                        periodOfStudy={"Jan 1016 - Dec 2021"}
-                        rightTitle={"Certificate Of Web Training"}
-                        rightTitleDescription={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet dapibus nibh ut faucibus nunc, egestas id amet porttitor. Pulvinar quisque sed amet, nulla nunc. Eleifend sodales posuere fusce tempus etiam et pellentesque. Molestie risus enim neque eget dui."}/>
+
+                    {workHistoryData.map((w) => {
+                        return (
+                            <EducationAndWorkHistoryCard
+                                leftTitle={w.leftTitle}
+                                status={w.status}
+                                periodOfStudy={w.periodOfStudy}
+                                rightTitle={w.rightTitle}
+                                rightTitleDescription={w.rightTitleDescription}/>
+                        )
+                    })}
+
                 </WrapperEducationAndWorkHistoryCards>
             </Container>
         </StyledSection>
