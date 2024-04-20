c97543f85845535ce4277eba817e910bf6a5bfdf
MargaritaZh <margaritazhitkevich@gmail.com>
2024-04-20 14:11:40 +0300

    Refactoring (map) - Education

--- src/assets/layout/sections/education/Education.tsx
+++ src/assets/layout/sections/education/Education.tsx
@@ -9,7 +9,31 @@ import {SectionTitle} from "../../../../components/SectionTitle";
 import {WrapperEducationAndWorkHistoryCards} from "../../../../components/WrapperEducationAndWorkHistoryCards";
 import {Container} from "../../../../components/Container";
 
-export const Education = () => {
+
+const educationData = [
+    {
+        leftTitle: "University of Toronto",
+        status: "Student",
+        periodOfStudy: "Jan 1016 - Dec 2021",
+        rightTitle: "Certificate Of Web Training",
+        rightTitleDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet dapibus nibh ut faucibus nunc, egestas id amet porttitor. Pulvinar quisque sed amet, nulla nunc. Eleifend sodales posuere fusce tempus etiam et pellentesque. Molestie risus enim neque eget dui.",
+    },
+    {
+        leftTitle: "Programming Course",
+        status: "Student",
+        periodOfStudy: "Jan 1016 - Dec 2021",
+        rightTitle: "Certificate Of Web Training",
+        rightTitleDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet dapibus nibh ut faucibus nunc, egestas id amet porttitor. Pulvinar quisque sed amet, nulla nunc. Eleifend sodales posuere fusce tempus etiam et pellentesque. Molestie risus enim neque eget dui.",
+    }, {
+        leftTitle: "Web developer courses",
+        status: "Student",
+        periodOfStudy: "Jan 1016 - Dec 2021",
+        rightTitle: "Certificate Of Web Training",
+        rightTitleDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet dapibus nibh ut faucibus nunc, egestas id amet porttitor. Pulvinar quisque sed amet, nulla nunc. Eleifend sodales posuere fusce tempus etiam et pellentesque. Molestie risus enim neque eget dui.",
+    },
+]
+
+export const Education:React.FC = () => {
     return (
         <StyledSection>
             <Container>
@@ -21,27 +45,19 @@ export const Education = () => {
                     </SectionDescription>
                 </SectionTitle>
                 <WrapperEducationAndWorkHistoryCards>
-                    <EducationAndWorkHistoryCard
-                        leftTitle={"University of Toronto"}
-                        status={"Student"}
-                        periodOfStudy={"Jan 1016 - Dec 2021"}
-                        rightTitle={"Certificate Of Web Training"}
-                        rightTitleDescription={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet dapibus nibh ut faucibus nunc, egestas id amet porttitor. Pulvinar quisque sed amet, nulla nunc. Eleifend sodales posuere fusce tempus etiam et pellentesque. Molestie risus enim neque eget dui."}
-                    />
-                    <EducationAndWorkHistoryCard
-                        leftTitle={"Programming Course"}
-                        status={"Student"}
-                        periodOfStudy={"Jan 1016 - Dec 2021"}
-                        rightTitle={"Certificate Of Web Training"}
-                        rightTitleDescription={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet dapibus nibh ut faucibus nunc, egestas id amet porttitor. Pulvinar quisque sed amet, nulla nunc. Eleifend sodales posuere fusce tempus etiam et pellentesque. Molestie risus enim neque eget dui."}
-                    />
-                    <EducationAndWorkHistoryCard
-                        leftTitle={"Web developer courses"}
-                        status={"Student"}
-                        periodOfStudy={"Jan 1016 - Dec 2021"}
-                        rightTitle={"Certificate Of Web Training"}
-                        rightTitleDescription={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet dapibus nibh ut faucibus nunc, egestas id amet porttitor. Pulvinar quisque sed amet, nulla nunc. Eleifend sodales posuere fusce tempus etiam et pellentesque. Molestie risus enim neque eget dui."}
-                    />
+
+                    {educationData.map((e)=>{
+                        return(
+                            <EducationAndWorkHistoryCard
+                                leftTitle={e.leftTitle}
+                                status={e.status}
+                                periodOfStudy={e.periodOfStudy}
+                                rightTitle={e.rightTitle}
+                                rightTitleDescription={e.rightTitleDescription}
+                            />
+                        )
+                    })}
+
                 </WrapperEducationAndWorkHistoryCards>
             </Container>
         </StyledSection>
